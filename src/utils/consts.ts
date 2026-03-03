095e13b2c1d9b2aa7393bbb9407c8ac677134b63
farzad <236311068+farzadatdointl@users.noreply.github.com>
2026-03-03 15:23:47 +0100

    chore: bump version 0.3.0 -> 0.4.0

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,3 +1,3 @@
-export const SERVER_VERSION = "0.3.0";
+export const SERVER_VERSION = "0.4.0";
 export const SERVER_NAME = "doit-mcp-server";
 export const SERVER_NAME_WEB = "Doit"; // for backwards compatibility, consistent name
